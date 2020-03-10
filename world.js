b2e020cd356d19e83e3a45f8ded58f99324894b1
n3op2 <paulius.michelevicius@gmail.com>
2020-03-10 22:57:33 +0000

    World and creature.

--- world.js
+++ world.js
@@ -44,6 +44,16 @@ Grid.prototype.set = function(vector, val) {
   return this.space[vector.x + this.width * vector.y] = val;
 }
 
+Grid.prototype.forEach = function(f, context) {
+  for (let y = 0; y < this.height; y++) {
+    for (let x = 0; x < this.width; x++) {
+      let val = this.space[x + y * this.width];
+      if (val != null)
+        f.call(context, val, new Vector(x, y));
+    }
+  }
+}
+
 const directions = {
   "n": new Vector(0, -1),
   "ne": new Vector(1, -1),
@@ -81,7 +91,7 @@ function elementFromChar(legend, ch) {
 
 function World(map, legend) {
   var grid = new Grid(map[0].length, map.length);
-  this.grid = grid;
+  this.grid = grid; 
   this.legend = legend;
 
   map.forEach(function(line, y) {
@@ -101,6 +111,35 @@ function charFromElement(element) {
     return element.originChar;
 }
 
+function View(world, vector) {
+  this.world = world;
+  this.vector = vector;
+}
+
+View.prototype.look = function(dir) {
+  let target = this.vector.plus(directions[dir]);
+  if (this.world.grid.isInside(target))
+    return charFromElement(this.world.grid.get(target));
+  else
+    return '#';
+}
+
+View.prototype.findAll = function(ch) {
+  let found = [];
+  for (var dir in directions) {
+    if (this.look(dir) === ch) {
+      found.push(dir);
+    }
+  }
+  return found;
+}
+
+View.prototype.find = function(ch) {
+  let found = this.findAll(ch);
+  if (found.length === 0) return null;
+  return rndEl(found);
+}
+
 World.prototype.print = function() {
   let output = '';
   for (let y = 0; y < this.grid.height; y++) {
@@ -113,10 +152,45 @@ World.prototype.print = function() {
   return output;
 }
 
+World.prototype.turn = function() {
+  let acted = [];
+  this.grid.forEach(function(creature, vector) {
+    if (creature.act && acted.indexOf(creature) === -1) {
+      console.log(creature);
+      acted.push(creature);
+      this.letAct(creature, vector);
+    }
+  }, this);
+}
+
+World.prototype.checkDestination = function(action, vector) {
+  if (directions.hasOwnProperty(action.direction)) {
+    let dest = vector.plus(directions[action.direction]);
+    console.log(dest);
+    if (this.grid.isInside(dest))
+      return dest;
+  }
+}
+
+World.prototype.letAct = function(creature, vector) {
+  let action = creature.act(new View(this, vector));
+  if (action && action.type === 'move') {
+    let dest = this.checkDestination(action, vector);
+    console.log(creature, vector, dest)
+    if (dest && this.grid.get(dest) == null) {
+      this.grid.set(vector, null);
+      this.grid.set(dest, creature);
+    }
+  }
+}
+
 const world = new World(plan, {
   "#": Wall,
   "o": BouncingCreature
 });
 
-console.log(world.print());
+for (let i = 0; i < 10; i++) {
+  world.turn();
+  console.log(world.print());
+}
 
